7108db5a4ae815a9d378df99044bd69075072bfe
BloodyLord02 <gamehag4304@gmail.com>
2025-11-10 19:41:21 +0200

    lab3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   const {page, grid, score, startGame, finishGame, restartGame, updateCell, initialGrid, error, giveHint,} = useSudokuGame()
 
   if (page === "start") return <StartPage onStart={startGame} />
-  if (page === "game") return ( <GamePage grid={grid} onFinish={finishGame} onChange={updateCell} initialGrid={initialGrid} error={error} onHint={giveHint}  />)
+  if (page === "game") return  <GamePage grid={grid} onFinish={finishGame} onChange={updateCell} initialGrid={initialGrid} error={error} onHint={giveHint}  />
   if (page === "result") return <ResultPage score={score} onRestart={restartGame} />
   return null
 }
