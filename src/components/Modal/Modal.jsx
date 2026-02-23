15f2e8a37a02ff926deaac8c7c6e73d0aa6b9ae0
BloodyLord02 <gamehag4304@gmail.com>
2026-02-23 19:23:53 +0200

    Add Docs

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,18 +1,34 @@
 import ReactDOM from "react-dom"
 import "./Modal.css"
 
-export default function Modal({ isOpen, onClose, onRestart, score }) {
+export default function Modal({ isOpen, type, score, onRestart, onClose }) {
   if (!isOpen) return null
 
   return ReactDOM.createPortal(
     <div className="modal-overlay">
       <div className="modal-window">
-        <h2>Гру завершено!</h2>
-        <p>Ваш результат: {score}</p>
-        <div className="buttons">
-          <button onClick={onRestart}>Почати заново</button>
-          <button onClick={onClose}>Закрити</button>
-        </div>
+
+        {type === "finish" && (
+          <>
+            <h2>Гру завершено!</h2>
+            <p>Ваш результат: {score}</p>
+            <div className="buttons">
+              <button onClick={onRestart}>Почати заново</button>
+              <button onClick={onClose}>Закрити</button>
+            </div>
+          </>
+        )}
+
+        {type === "menu" && (
+          <>
+            <h2>Меню гри</h2>
+            <div className="buttons">
+              <button onClick={onRestart}>Почати нову гру</button>
+              <button onClick={onClose}>Продовжити гру</button>
+            </div>
+          </>
+        )}
+
       </div>
     </div>,
     document.body
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -4,27 +4,33 @@ import Button from "../components/Button/Button"
 import Modal from "../components/Modal/Modal"
 import { useState } from "react"
 
-export default function GamePage({ onFinish, grid, onChange, initialGrid, error, onHint,}) {
-  const [showModal, setShowModal] = useState(false)
+export default function GamePage({ onFinish, onRestart, grid, onChange, initialGrid, error, onHint }) {
+  const [modalType, setModalType] = useState(null)
   const [lastScore, setLastScore] = useState(0)
-
+  
   const handleFinish = () => {
     const score = onFinish()
-    if (score !== false) { 
+    if (score !== false) {
       setLastScore(score)
-      setShowModal(true)
+      setModalType("finish")
     }
   }
+
   return (
     <div className="page top-page">
       <Header title="Sudoku Game" />
+
       <SudokuGrid grid={grid} onChange={onChange} initialGrid={initialGrid} />
+
       {error && <p style={{ color: "red", marginTop: "10px" }}>{error}</p>}
+
       <div className="mt-20 flex flex-col items-center gap-3">
         <Button onClick={handleFinish}>Завершити</Button>
         <Button onClick={onHint}>Підказка</Button>
+        <Button onClick={() => setModalType("menu")}>Меню</Button>
       </div>
-      <Modal isOpen={showModal} score={lastScore} onRestart={() => window.location.reload()} onClose={() => setShowModal(false)}/>
+
+      <Modal isOpen={modalType !== null} type={modalType} score={lastScore} onRestart={() => window.location.reload()} onClose={() => setModalType(null)}/>
     </div>
   )
 }
